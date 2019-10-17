ceeb3a15b96a9bafe3bdf183d886453513f4d526
Turi-byte <breldanmuturi001@gmail.com>
2019-10-17 22:07:53 +0300

    Uri Builder

--- app/src/main/java/turi/manzi/manzikotlin/MainActivity.kt
+++ app/src/main/java/turi/manzi/manzikotlin/MainActivity.kt
@@ -1,5 +1,7 @@
 package turi.manzi.manzikotlin
 
+import android.net.Uri
+import android.nfc.NdefRecord.createUri
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -11,22 +13,48 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity(),GetRawData.OnDownloadComplete, GetFlickrJsonData.OnDataAvailable {
+class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
+    GetFlickrJsonData.OnDataAvailable {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG, "onCreate called" )
+        Log.d(TAG, "onCreate called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val url = createUri(
+            "https://api.flickr.com/services/feeds/photos_public.gne",
+            "android,oreo",
+            "en-us",
+            true
+        )
         val getRawData = GetRawData(this)
-        getRawData.execute("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,oreo&format=json&nojsoncallback=1")
+        getRawData.execute(url)
         Log.d(TAG, "onCreate ends")
+    }
+
+    private fun createUri(
+        baseURL: String,
+        searchCriteria: String,
+        lang: String,
+        matchAll: Boolean
+    ): String {
+        Log.d(TAG, ".createUri starts")
+        return Uri.parse(baseURL)
+            .buildUpon()
+            .appendQueryParameter("tags", searchCriteria)
+            .appendQueryParameter("tagmode", if (matchAll) "ALL" else "ANY")
+            .appendQueryParameter("lang", lang)
+            .appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1")
+            .build()
+            .toString()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        Log.d(TAG, "onCreateOptionsMenu called" )
+        Log.d(TAG, "onCreateOptionsMenu called")
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
@@ -35,34 +63,35 @@ class MainActivity : AppCompatActivity(),GetRawData.OnDownloadComplete, GetFlick
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        Log.d(TAG, "onOptionsItemSelected called" )
+        Log.d(TAG, "onOptionsItemSelected called")
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
     //    companion object {
 //        private const val TAG ="MainActivity"
 //    }
-    override fun onDownloadComplete(data: String, status: DownloadStatus){
-        if(status == DownloadStatus.OK) {
-            Log.d(TAG,"onDownloadComplete called")
+    override fun onDownloadComplete(data: String, status: DownloadStatus) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete called")
             val getFlickrJsonData = GetFlickrJsonData(this)
             getFlickrJsonData.execute(data)
         } else {
-            Log.d(TAG,"onDownloadComplete failed with status $status. Error message is $data")
+            Log.d(TAG, "onDownloadComplete failed with status $status. Error message is $data")
         }
 
 
     }
 
     override fun onDataAvailable(data: List<Photo>) {
-        Log.d(TAG,".onDataAvailable called, data is $data")
+        Log.d(TAG, ".onDataAvailable called, data is $data")
 
-        Log.d(TAG,".onDataAvailable ends")
+        Log.d(TAG, ".onDataAvailable ends")
     }
 
     override fun onError(exception: Exception) {
-        Log.d(TAG,".onError called with ${exception.message}")
+        Log.d(TAG, ".onError called with ${exception.message}")
     }
 }
--- app/src/main/java/turi/manzi/manzikotlin/Photo.kt
+++ app/src/main/java/turi/manzi/manzikotlin/Photo.kt
@@ -1,8 +1,8 @@
 package turi.manzi.manzikotlin
 
-class Photo (val title: String, val author: String, val link: String, val tags: String, val image: String, val photoUrl: String){
+class Photo (val title: String, val author: String,val authorId: String, val link: String, val tags: String, val image: String){
 
     override fun toString(): String {
-        return "Photo(title='$title', author='$author', link='$link', tags='$tags', image='$image', photoUrl='$photoUrl')"
+        return "Photo(title='$title', author='$author',authorId='$authorId', link='$link', tags='$tags', image='$image')"
     }
 }
