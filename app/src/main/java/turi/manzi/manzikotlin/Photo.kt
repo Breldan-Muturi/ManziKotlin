2d7da2ea1e18353ea28bdee1d1ea749eef60b862
Turi-byte <breldanmuturi001@gmail.com>
2019-10-17 22:22:03 +0300

    Bug fix

--- app/src/main/java/turi/manzi/manzikotlin/Photo.kt
+++ app/src/main/java/turi/manzi/manzikotlin/Photo.kt
@@ -1,12 +1,8 @@
 package turi.manzi.manzikotlin
 
-<<<<<<< HEAD
-
-=======
 class Photo(val title: String, val author: String,val authorId: String, val link: String, val tags: String, val image: String){
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
->>>>>>> JSONData
-    }
+   }
 }
