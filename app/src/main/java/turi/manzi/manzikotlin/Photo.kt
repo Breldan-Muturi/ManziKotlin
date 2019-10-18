463f785d93c71020ac34cbb101328a1598e36a8b
Turi-byte <breldanmuturi001@gmail.com>
2019-10-18 22:05:24 +0300

    Serializable

--- app/src/main/java/turi/manzi/manzikotlin/Photo.kt
+++ app/src/main/java/turi/manzi/manzikotlin/Photo.kt
@@ -1,8 +1,47 @@
 package turi.manzi.manzikotlin
 
-class Photo(val title: String, val author: String,val authorId: String, val link: String, val tags: String, val image: String){
+import android.util.Log
+import java.io.IOException
+import java.io.ObjectStreamException
+import java.io.Serializable
+
+class Photo(
+    var title: String, var author: String, var authorId: String, var link: String, var tags: String,
+    var image: String
+) : Serializable {
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tags='$tags', image='$image')"
-   }
+    }
+
+    @Throws(IOException::class)
+    private fun writeObject(out: java.io.ObjectOutputStream) {
+        Log.d("Photo", "writeObject called")
+        out.writeUTF(title)
+        out.writeUTF(author)
+        out.writeUTF(authorId)
+        out.writeUTF(link)
+        out.writeUTF(tags)
+        out.writeUTF(image)
+    }
+
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun readObject(inStream: java.io.ObjectInputStream) {
+        Log.d("Photo", "readObject called")
+        title = inStream.readUTF()
+        author = inStream.readUTF()
+        authorId = inStream.readUTF()
+        link = inStream.readUTF()
+        tags = inStream.readUTF()
+        image = inStream.readUTF()
+    }
+
+    @Throws(ObjectStreamException::class)
+    private fun readObjectNoData(){
+        Log.d("Photo","readObjectNoData called")
+    }
 }
