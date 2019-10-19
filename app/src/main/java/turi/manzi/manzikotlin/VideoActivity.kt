429c6353b2d7d84d47f15c64f467510a542ea8ab
Turi-byte <breldanmuturi001@gmail.com>
2019-10-20 01:50:19 +0300

    Add YouTube player widget dynamically

--- app/src/main/java/turi/manzi/manzikotlin/VideoActivity.kt
+++ app/src/main/java/turi/manzi/manzikotlin/VideoActivity.kt
@@ -1,15 +1,29 @@
 package turi.manzi.manzikotlin
 
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video)
+//        setContentView(R.layout.activity_video)
+//        val layout = findViewById<ConstraintLayout>(R.id.activity_video)
+        val layout = layoutInflater.inflate(R.layout.activity_video, null) as ConstraintLayout
+        setContentView(layout)
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180)
+//        button1.text = "Button Added"
+//        layout.addView(button1)
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(
