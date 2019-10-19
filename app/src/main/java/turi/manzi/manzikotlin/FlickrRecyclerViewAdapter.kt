0d4c900950ad4a492894ce7de4d7e3af649e5745
Turi-byte <breldanmuturi001@gmail.com>
2019-10-19 22:54:59 +0300

    Show placeholder when searchCriteria connot be found

--- app/src/main/java/turi/manzi/manzikotlin/FlickrRecyclerViewAdapter.kt
+++ app/src/main/java/turi/manzi/manzikotlin/FlickrRecyclerViewAdapter.kt
@@ -10,35 +10,44 @@ import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
 private const val TAG = "FlickrRecyclerViewAdapt"
-class FlickrImageViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+class FlickrImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var thumbnail: ImageView = view.findViewById(R.id.thumbnail)
     var title: TextView = view.findViewById(R.id.title)
 
 }
-class FlickrRecyclerViewAdapter(private var photoList: List<Photo   >) : RecyclerView.Adapter<FlickrImageViewHolder>() {
+
+class FlickrRecyclerViewAdapter(private var photoList: List<Photo>) :
+    RecyclerView.Adapter<FlickrImageViewHolder>() {
     override fun getItemCount(): Int {
 //        Log.d(TAG, ".getItemCount called")
-        return if (photoList.isNotEmpty()) photoList.size else 0
+        return if (photoList.isNotEmpty()) photoList.size else 1
     }
 
-    fun loadNewData(newPhotos: List<Photo>){
+    fun loadNewData(newPhotos: List<Photo>) {
         photoList = newPhotos
         notifyDataSetChanged()
     }
 
-    fun getPhoto(position: Int): Photo?{
+    fun getPhoto(position: Int): Photo? {
         return if (photoList.isNotEmpty()) photoList[position] else null
     }
 
     override fun onBindViewHolder(holder: FlickrImageViewHolder, position: Int) {
 //        Called by the layout manager when it wants new data in an existing view
-        val photoItem = photoList[position]
+        if (photoList.isEmpty()) {
+            holder.thumbnail.setImageResource(R.drawable.placeholder)
+            holder.title.setText(R.string.search_fail)
+                "No photos match Search Criteria.\n\n Use search icon to search for Photos"
+        } else {
+            val photoItem = photoList[position]
 //        Log.d(TAG, "onBindViewHolder: ${photoItem.title} --> $position")
-        Picasso.get().load(photoItem.image)
-            .error(R.drawable.placeholder)
-            .placeholder(R.drawable.placeholder)
-            .into(holder.thumbnail)
-        holder.title.text = photoItem.title
+            Picasso.get().load(photoItem.image)
+                .error(R.drawable.placeholder)
+                .placeholder(R.drawable.placeholder)
+                .into(holder.thumbnail)
+            holder.title.text = photoItem.title
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlickrImageViewHolder {
