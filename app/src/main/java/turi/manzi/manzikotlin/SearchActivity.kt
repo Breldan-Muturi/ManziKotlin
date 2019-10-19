2d6530aaa29f31ce306128db5f66ce8b3a726cbf
Turi-byte <breldanmuturi001@gmail.com>
2019-10-19 10:13:01 +0300

    Search View

--- app/src/main/java/turi/manzi/manzikotlin/SearchActivity.kt
+++ app/src/main/java/turi/manzi/manzikotlin/SearchActivity.kt
@@ -2,9 +2,12 @@ package turi.manzi.manzikotlin
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 
 private const val TAG = "SearchActivity"
 class SearchActivity : BaseActivity() {
+    private var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG,".onCreate starts")
@@ -14,4 +17,8 @@ class SearchActivity : BaseActivity() {
         Log.d(TAG,".onCreate ends")
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        return true
+    }
 }
