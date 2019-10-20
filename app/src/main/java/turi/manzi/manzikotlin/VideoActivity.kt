a9b65773ec284e39b1c61cdf09b0359442960c6e
Turi-byte <breldanmuturi001@gmail.com>
2019-10-20 12:05:39 +0300

    Configure the YouTube Player

--- app/src/main/java/turi/manzi/manzikotlin/VideoActivity.kt
+++ app/src/main/java/turi/manzi/manzikotlin/VideoActivity.kt
@@ -2,6 +2,7 @@ package turi.manzi.manzikotlin
 
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
@@ -27,6 +28,8 @@ class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener
         playerView.layoutParams = ConstraintLayout.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT)
         layout.addView(playerView)
+
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY), this)
     }
 
     override fun onInitializationSuccess(
@@ -38,9 +41,15 @@ class VideoActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener
     }
 
     override fun onInitializationFailure(
-        p0: YouTubePlayer.Provider?,
-        p1: YouTubeInitializationResult?
+        provider: YouTubePlayer.Provider?,
+        youTubeInitializationResult: YouTubeInitializationResult?
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val REQUEST_CODE = 0
+        if(youTubeInitializationResult?.isUserRecoverableError == true){
+            youTubeInitializationResult.getErrorDialog(this,REQUEST_CODE).show()
+        } else{
+            val errorMessage = "There was an error initializing youTubePlayer"
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
